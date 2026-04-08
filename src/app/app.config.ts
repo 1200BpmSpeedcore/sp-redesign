11c71bf9bd6bd8339523319450016ddcc9b96093
Tom Brauer <tombrauer0102@gmail.com>
2026-04-08 06:40:10 +0200

    Add multiple languages support
    * Added i18n support for English and German languages.
    * Simple tests for app skeleton.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+
+import { provideTranslateService, TranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      })
+    }),
+    provideAppInitializer(() => {
+      const translate = inject(TranslateService);
+      translate.use(translate.getBrowserLang() || 'en');
+    })
   ]
 };
